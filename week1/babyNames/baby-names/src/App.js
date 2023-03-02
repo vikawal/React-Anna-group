deb7b4ad1ee8346fb56ad14307ba47ec098dda7a
vikawal <108365967+vikawal@users.noreply.github.com>
2023-03-03 00:06:17 +0100

    added filtering by gender and sorting in alphabet order

--- week1/babyNames/baby-names/src/App.js
+++ week1/babyNames/baby-names/src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
 import "./App.css";
 import {names} from "./baby-names-data.js";
+import NameColor from "./NameColor";
 import NamesList from "./NameList";
-import NameColor from "./NameColor.js";
+import {sortByName, filterByGender} from './sorting.js'
 
 
 function App() {
+  const [gender, setGender] = useState("");
+  const sortedNames = sortByName(names);
+  const filteredNames = gender ? filterByGender (sortedNames, gender): sortedNames;
   return (
     <div className="App">
-      <NamesList names={names} />
+      <div className="buttons">
+      <button onClick={() => setGender("")}>All</button>
+        <button onClick={() => setGender("f")}>Girls</button>
+        <button onClick={() => setGender("m")}>Boys</button>
+      </div>
+      <div className="names">
+       {filteredNames.map((item)=> (
+        <NameColor 
+        key= {item.id}
+        name={item.name}
+        sex={item.sex} />
+       )
+       )}
+      </div>
+    {/* <NamesList names={names} /> */}
       
     </div>
   );
