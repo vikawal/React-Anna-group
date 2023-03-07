7e9d613831263a9cb2945db24ca83f782a5e32a7
vikawal <108365967+vikawal@users.noreply.github.com>
2023-03-07 21:59:39 +0100

    added separate files with components

--- week1/project/EcommerceVika/ecommerce-shop-project/src/App.js
+++ week1/project/EcommerceVika/ecommerce-shop-project/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
+import Categories from "./components/Categories";
+import Products from "./components/Products";
+import FilteredProductsByCategory from "./components/FilteredProductsByCategory";
 import allCategories from "./fake-data/all-categories.js";
 import allProducts from "./fake-data/all-products.js";
 
@@ -10,44 +13,21 @@ function App() {
     console.log(`Clicked category: ${category}`);
     setSelectedCategory(category);
   };
-
-  const filteredProducts = selectedCategory
-    ? allProducts.filter(
-        (product) => product.category === selectedCategory.split(":")[1].trim()
-      )
-    : allProducts;
+  
+  const filteredProducts = FilteredProductsByCategory(allProducts, selectedCategory);
 
   return (
     <div className="App">
-      <div className="categories">
-        {allCategories.map((category) => (
-          <button
-            key={category}
-            onClick={() => handleCategoryClick(category)}
-            className={selectedCategory === category ? "active" : ""}
-          >
-            {category}
-          </button>
-        ))}
-        <button onClick={() => setSelectedCategory("")}>All Products</button>
-      </div>
+      <Categories
+        categories={allCategories}
+        selectedCategory={selectedCategory}
+        handleCategoryClick={handleCategoryClick}
+        />
+        <Products 
+        filteredProducts={filteredProducts}
+        />
 
-      <div className="products">
-        {filteredProducts.map((product) => (
-          <div key={product.id} className="product">
-            <img src={product.image} alt={product.title} />
-            <div className="product-details">
-              <div className="product-title">{product.title}</div>
-              <div className="product-price">{product.price}</div>
-              <div className="product-description">{product.description}</div>
-              <div className="product-category">{product.category}</div>
-              <div className="product-rating">
-                Rating: {product.rating.rate} ({product.rating.count} reviews)
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+        
     </div>
   );
 }
