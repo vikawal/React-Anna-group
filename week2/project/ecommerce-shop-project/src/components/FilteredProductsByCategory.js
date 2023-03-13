757a9068fafbc4063a004e70d10b2de794c25787
vikawal <108365967+vikawal@users.noreply.github.com>
2023-03-13 13:34:42 +0100

    "deleted not used files"

--- week2/project/ecommerce-shop-project/src/components/FilteredProductsByCategory.js
+++
@@ -1,9 +0,0 @@
-
-  const filteredProductsByCategory = (products, category) => {
-  return category ? products.filter(
-        (product) => product.category === category.split(":")[1].trim()
-      )
-    : products;
- };
-  
-export default filteredProductsByCategory;
