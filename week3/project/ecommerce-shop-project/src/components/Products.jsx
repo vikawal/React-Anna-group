e38e0047c3ac35a404b30bc48d6bc36b5a493ca7
vikawal <108365967+vikawal@users.noreply.github.com>
2023-03-28 01:21:37 +0200

    should work without mistakes now, before - 50/50

--- week3/project/ecommerce-shop-project/src/components/Products.jsx
+++ week3/project/ecommerce-shop-project/src/components/Products.jsx
@@ -2,16 +2,15 @@ import React from "react";
 import "./products.css";
 import Product from "./Product.jsx";
 
-
 function Products({ products: {products}, selectedCategory }) {
   console.log("Products:", products); 
-  const filteredProducts = selectedCategory
-  ? products.filter((product) => product.category === selectedCategory)
-  : products;
+  const filteredProducts = products && selectedCategory
+    ? products.filter((product) => product.category === selectedCategory)
+    : products;
   console.log("Filtered products:", filteredProducts);
   return (
     <div className="products">
-      {filteredProducts.length > 0 ? (
+      {filteredProducts && filteredProducts.length > 0 ? (
         filteredProducts.map((product) => (
           <Product product={product} key={product.id} />
         ))
@@ -21,5 +20,24 @@ function Products({ products: {products}, selectedCategory }) {
     </div>
   );
 }
+
+// function Products({ products: {products}, selectedCategory }) {
+//   console.log("Products:", products); 
+//   const filteredProducts = selectedCategory
+//   ? products.filter((product) => product.category === selectedCategory)
+//   : products;
+//   console.log("Filtered products:", filteredProducts);
+//   return (
+//     <div className="products">
+//       {filteredProducts.length > 0 ? (
+//         filteredProducts.map((product) => (
+//           <Product product={product} key={product.id} />
+//         ))
+//       ) : (
+//         <p> No products were found in the selected category. Try another category</p>
+//       )}
+//     </div>
+//   );
+// }
 
 export default Products;
