82626d760745620ea5062f394b2a863480ad46f0
vikawal <108365967+vikawal@users.noreply.github.com>
2023-03-29 21:17:59 +0200

    useFetch in ProductDetail added and some css

--- week3/project/ecommerce-shop-project/src/components/ProductDetail.jsx
+++ week3/project/ecommerce-shop-project/src/components/ProductDetail.jsx
@@ -1,50 +1,15 @@
 
-import React, {useState, useEffect} from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import "./product.css";
 import ProductAllInfo from "./ProductInfoAll"
+import useFetch from "./useFetchHook";
 
 function ProductDetail () {
   const {id} = useParams();
-  const [product, setProduct] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    fetch (`https://dummyjson.com/products/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      setProduct(data);
-      setLoading(false);
-    })
-    .catch((error) => {
-    console.log(error);
-    setLoading(false);
-    let errorCode = "";
-          if (error.response && error.response.status) {
-            errorCode = error.response.status;
-          } else if (error.request) {
-            errorCode = "NETWORK_ERROR";
-          } else {
-            errorCode = "UNKNOWN_ERROR";
-          }
-          switch (errorCode) {
-            case 404:
-              setError("Product data not found.");
-              break;
-            case "NETWORK_ERROR":
-              setError("Network error. Please check your internet connection and try again.");
-              break;
-            case "UNKNOWN_ERROR":
-              setError("Unknown error occurred. Please try again later.");
-              break;
-            default:
-              setError("Failed to load product data. Please, try again later");
-          }   
-    });
-  }, [id]);
-
-  console.log(id);
+  const {data: product, loading, error } = useFetch(
+    `https://dummyjson.com/products/${id}`
+  );
 
   return (
     <div className="productDetail">
